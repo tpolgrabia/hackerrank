ec6ee5042da874bc886ba2e31534013bac518b26
Tomasz Półgrabia <tpsa.s.a@gmail.com>
2017-01-09 21:52:12 +0100

    WTF solution for coin change. The whole change is to migration from int to unsigned long. WTF!!!!!!!!!!!!!

--- coin-change/coinchange.cpp
+++ coin-change/coinchange.cpp
@@ -2,61 +2,60 @@
 
 using namespace std;
 
-void dump_mat(int **arr, int m, int n) {
-    for (int i = 0; i <= m; i++) {
-	for (int j = 0; j <= n; j++) {
-	    cout << arr[i][j] << " ";
-	}
-	cout << endl;
+void dump_mat(unsigned long **arr, unsigned long m, unsigned long n) {
+    for (unsigned long i = 0; i <= m; i++) {
+        for (unsigned long j = 0; j <= n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
 }
 
 int main() {
-    int m;
-    int n;
+    unsigned long m;
+    unsigned long n;
 
     cin >> n;
     cin >> m;
-    int *coins = new int[m+1];
+    unsigned long *coins = new unsigned long[m+1];
     coins[0] = 0;
-    for (int i = 1; i <= m; i++) {
+    for (unsigned long i = 1; i <= m; i++) {
         cin >> coins[i];
     }
 
-    int **exchange_ways = new int*[m+1];
-    for (int i = 0; i <= m; i++) {
-	exchange_ways[i] = new int[n+1];
+    unsigned long **exchange_ways = new unsigned long*[m+1];
+    for (unsigned long i = 0; i <= m; i++) {
+	    exchange_ways[i] = new unsigned long[n+1];
     }
-    
 
-    for (int i = 0; i <= n; i++) {
-	exchange_ways[0][i] = 0;
+    for (unsigned long i = 0; i <= n; i++) {
+    	exchange_ways[0][i] = 0;
     }
     // cout << "Starting" << endl;
     exchange_ways[0][0] = 1;
 
-    for (int i = 1; i <= m; i++) {
+    for (unsigned long i = 1; i <= m; i++) {
 	// we are choosing the coin right now <= i (subset)
 	// cout << "Loop i: " << i << endl;
-	for (int j = 0; j <= n; j++) {
-	    // we are solving problem for every coin value
-	    // cout << "Loop j: " << j << endl;
-	    int combinations = 0;
-	    for (int k = 0; j - k*coins[i] >= 0; k++) {
-		// cout << "I: " << i << ", J: " << j << ", K: " << k << endl;
-		int val = exchange_ways[i-1][j - k*coins[i]];
-		/*		
-		if (val > 0) {
-		    cout << "N: " << j << ", K: " << k
-			 << ", exchange: " << j - k*coins[i]
-			 << ", cidx: " << i
-			 << ", coin: " << coins[i] << endl;
-		}
-		*/
-		combinations += val; 
-	    }
-	    exchange_ways[i][j] = combinations;
-	}
+        for (unsigned long j = 0; j <= n; j++) {
+            // we are solving problem for every coin value
+            // cout << "Loop j: " << j << endl;
+            unsigned long combinations = 0;
+            for (unsigned long k = 0; j >= k*coins[i]; k++) {
+                // cout << "I: " << i << ", J: " << j << ", K: " << k << endl;
+                unsigned long val = exchange_ways[i-1][j - k*coins[i]];
+                /*
+                if (val > 0) {
+                    cout << "N: " << j << ", K: " << k
+                     << ", exchange: " << j - k*coins[i]
+                     << ", cidx: " << i
+                     << ", coin: " << coins[i] << endl;
+                }
+                */
+                combinations += val;
+            }
+            exchange_ways[i][j] = combinations;
+        }
     }
 
     // cout << "Finished" << endl;
@@ -64,10 +63,9 @@ int main() {
 
     cout << exchange_ways[m][n] << endl;
 
-    for (int i = 0; i <= m; i++) {
-	delete[] exchange_ways[i];
+    for (unsigned long i = 0; i <= m; i++) {
+	    delete[] exchange_ways[i];
     }
-    
     delete[] exchange_ways;
     delete[] coins;
     return 0;
